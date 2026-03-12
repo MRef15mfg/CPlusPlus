43d80d9636df7a6eb99bf96670ffba72319f8315
MRef15mfg <mrefmfg@gmail.com>
2026-03-12 18:10:43 +0100

    wqer

--- CppSrp28/task1.cpp
+++ CppSrp28/task1.cpp
@@ -1,120 +1,114 @@
 #include <iostream>
-#include <string>
-using namespace std;
 
-// ==================== Завдання №1 ====================
-
-class Student {
-protected:
-    string name;
-    int age;
-    string faculty;
-    double averageGrade;
-
-public:
-    Student(string n, int a, string f, double avg)
-        : name(n), age(a), faculty(f), averageGrade(avg) {}
-
-    virtual void showInfo() const {
-        cout << "Ім'я: " << name << endl;
-        cout << "Вік: " << age << endl;
-        cout << "Факультет: " << faculty << endl;
-        cout << "Середній бал: " << averageGrade << endl;
-    }
-
-    virtual double getScholarship() const {
-        if (averageGrade >= 90)
-            return 2000;
-        else
-            return 1000;
-    }
-
-    virtual ~Student() {}
+template <typename T>
+struct Node {
+    T data;
+    Node* next;
+    Node(T val) : data(val), next(nullptr) {}
 };
 
-class Aspirant : public Student {
+template <typename T>
+class List {
 private:
-    string thesisTopic;
-
+    Node<T>* head;
 public:
-    Aspirant(string n, int a, string f, double avg, string topic)
-        : Student(n, a, f, avg), thesisTopic(topic) {}
+    List() : head(nullptr) {}
 
-    void showInfo() const override {
-        Student::showInfo();
-        cout << "Тема дисертації: " << thesisTopic << endl;
+    void push_front(T val) {
+        Node<T>* newNode = new Node<T>(val);
+        newNode->next = head;
+        head = newNode;
     }
 
-    double getScholarship() const override {
-        if (averageGrade >= 90)
-            return 3000;
-        else
-            return 2000;
+    void push_back(T val) {
+        Node<T>* newNode = new Node<T>(val);
+        if (!head) {
+            head = newNode;
+            return;
+        }
+        Node<T>* temp = head;
+        while (temp->next) temp = temp->next;
+        temp->next = newNode;
+    }
+
+    void pop_front() {
+        if (!head) return;
+        Node<T>* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    void printAll() const {
+        Node<T>* temp = head;
+        while (temp) {
+            std::cout << temp->data << " ";
+            temp = temp->next;
+        }
+        std::cout << std::endl;
+    }
+
+    bool isEmpty() const { return head == nullptr; }
+
+    ~List() {
+        while (!isEmpty()) pop_front();
     }
 };
 
-// ==================== Завдання №2 ====================
-
-class Passport {
-protected:
-    string fullName;
-    string birthDate;
-    string passportNumber;
-
+class Point {
 public:
-    Passport(string name, string date, string number)
-        : fullName(name), birthDate(date), passportNumber(number) {}
+    double x, y;
+    Point() : x(0), y(0) {}
+    Point(double x, double y) : x(x), y(y) {}
 
-    virtual void showPassportInfo() const {
-        cout << "ПІБ: " << fullName << endl;
-        cout << "Дата народження: " << birthDate << endl;
-        cout << "Номер паспорта: " << passportNumber << endl;
+    friend std::ostream& operator<<(std::ostream& os, const Point& p) {
+        os << "(" << p.x << ", " << p.y << ")";
+        return os;
     }
-
-    virtual ~Passport() {}
 };
 
-class ForeignPassport : public Passport {
+class Vector {
 private:
-    string foreignPassportNumber;
-    string visas;
+    List<Point> points;
+    int size;
 
 public:
-    ForeignPassport(string name, string date, string number,
-                    string foreignNumber, string visaInfo)
-        : Passport(name, date, number),
-          foreignPassportNumber(foreignNumber),
-          visas(visaInfo) {}
+    Vector() : size(0) {}
 
-    void showPassportInfo() const override {
-        Passport::showPassportInfo();
-        cout << "Номер закордонного паспорта: "
-             << foreignPassportNumber << endl;
-        cout << "Візи: " << visas << endl;
+    void addAtBeginning(Point p) {
+        points.push_front(p);
+        size++;
+    }
+
+    void addAtEnd(Point p) {
+        points.push_back(p);
+        size++;
+    }
+
+    void removeFirst() {
+        if (size > 0) {
+            points.pop_front();
+            size--;
+        }
+    }
+
+    void Print() const {
+        std::cout << "Vector (size " << size << "): ";
+        points.printAll();
     }
 };
-
 
 int main() {
+    Vector myVector;
 
-    cout << "===== Студент =====" << endl;
-    Student s("Іван Петренко", 19, "ІТ", 92);
-    s.showInfo();
-    cout << "Стипендія: " << s.getScholarship() << endl;
+    myVector.addAtEnd(Point(1.2, 3.4));
+    myVector.addAtEnd(Point(5.6, 7.8));
+    myVector.addAtBeginning(Point(0, 0));
 
-    cout << "\n===== Аспірант =====" << endl;
-    Aspirant a("Олена Коваль", 24, "Фізика", 95, "Квантова механіка");
-    a.showInfo();
-    cout << "Стипендія: " << a.getScholarship() << endl;
+    myVector.Print();
 
-    cout << "\n===== Паспорт =====" << endl;
-    Passport p("Іван Петренко", "01.01.2005", "АА123456");
-    p.showPassportInfo();
-
-    cout << "\n===== Закордонний паспорт =====" << endl;
-    ForeignPassport fp("Олена Коваль", "12.05.2000",
-                       "ВВ654321", "FP987654", "Шенген");
-    fp.showPassportInfo();
+    myVector.removeFirst();
+    std::cout << "After removing first element:" << std::endl;
+    myVector.Print();
 
     return 0;
 }
